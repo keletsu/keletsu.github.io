2aa05f6625e7ed83103042dc2ae74095160e09b7
keletsu <146950844+keletsu@users.noreply.github.com>
2024-09-14 13:47:38 +0500

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,43 @@
+const cacheName = "severstal"
+const cacheFiles = [
+    "/",
+    "/index.html",
+    "/logo.png",
+    "/assets/index.js",
+    "/assets/index.css",
+    "/assets/manifest.webmanifest",
+    "/model/model.json",
+    "/model/group1-shard1of7.bin",
+    "/model/group1-shard2of7.bin",
+    "/model/group1-shard3of7.bin",
+    "/model/group1-shard4of7.bin",
+    "/model/group1-shard5of7.bin",
+    "/model/group1-shard6of7.bin",
+    "/model/group1-shard7of7.bin"
+]
+
+self.addEventListener("install", (e) => {
+  console.log("Worker install");
+  e.waitUntil(
+      (async () => {
+        const cache = await caches.open(cacheName);
+        console.log("Caching all");
+        await cache.addAll(cacheFiles);
+      })(),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            console.log(`Fetching ${e.request.url}`);
+            if (r) return r;
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`Caching ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })(),
+    );
+});
